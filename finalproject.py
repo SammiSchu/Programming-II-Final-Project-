8f164222a9781ea04dd34058e8a50b7cc9dcb788
Adom Oshagan <oshgana@emmanuel.edu>
2023-05-03 15:31:45 -0400

    wordCloud WORKING

--- finalproject.py
+++ finalproject.py
@@ -1,21 +1,38 @@
-#Importing Libraries
-import pandas as pd
+#importing all necessary modules
+from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
-%matplotlib inline
-from wordcloud import WordCloud
-#Importing Dataset
+import pandas as pd
+
+# Reads 'Youtube04-Eminem.csv' file
 df = pd.read_csv("android-games.csv")
-#Checking the Data
-df.head()
-#Checking for NaN values
-df.isna().sum()
-#Removing NaN Values
-df.dropna(inplace = True)
-#Creating the text variable
-text = " ".join(cat.split()[1] for cat in df.category)
-# Creating word_cloud with text as argument in .generate() method
-word_cloud = WordCloud(collocations = False, background_color = 'white').generate(text)
-# Display the generated Word Cloud
-plt.imshow(wordcloud, interpolation='bilinear')
+
+comment_words = ''
+stopwords = set(STOPWORDS)
+
+# iterate through the csv file
+for val in df:
+	
+	# typecaste each val to string
+	val = str(val)
+
+	# split the value
+	tokens = val.split()
+	
+	# Converts each token into lowercase
+	for i in range(len(tokens)):
+		tokens[i] = tokens[i].lower()
+	
+	comment_words += " ".join(tokens)+" "
+
+wordcloud = WordCloud(width = 800, height = 800,
+				background_color ='white',
+				stopwords = stopwords,
+				min_font_size = 10).generate(comment_words)
+
+# plot the WordCloud image					
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud)
 plt.axis("off")
+plt.tight_layout(pad = 0)
+
 plt.show()
