d3bd75f087cff8b923e4906e618319e4a2688923
Sammi Schumacher <schumachers@emmanuel.edu>
2023-05-06 15:10:59 -0400

    trying, and failing, to filter article

--- finalproject.py
+++ finalproject.py
@@ -4,11 +4,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 import csv
+from functools import reduce
+import operator
 
-with open('tweet_words.json', encoding = 'utf-8') as inputfile:
-    tweet_words = pd.read_json(inputfile)
-tweet_words.to_csv('tweet_words.csv', encoding = 'utf-8', index = False)
-tweet_words = [str(i) for i in "tweet_words.csv"]
+def save_to_json_file(obj, filename):
+    f = open(filename, 'w')
+    json.dump(obj, f)
+    f.close()
+
+# with open('tweet_words.json', encoding = 'utf-8') as inputfile:
+#     tweet_words = pd.read_json(inputfile)
+# tweet_words.to_csv('tweet_words.csv', encoding = 'utf-8', index = False)
+# tweet_words = [str(i) for i in "tweet_words.csv"]
+
+def article_filter(df):
+    articles = ['a', 'an', 'the', 'it', 'is', 'its', 'they', 'them', 'their', 'our', 'all', 'any', 'another', 'I', 'both', 'each','we'
+                'us','some', 'anything', 'these', 'he', 'him', 'she', 'most', 'you', 'her', 'others', 'himself', 'where', 'whose', 'your']
+    if (df in articles):
+        return True
+    else:
+        return False
+    
+## attempting to save filtered words into a file 
+tweet_words_json = pd.read_json('tweet_words.json')
+filtered = filter(article_filter, tweet_words_json)
+for articles in filtered:
+    print(filtered)
+    # save_to_json_file(filtered, 'tweet_words_filtered.json')
 
 # Reads 'Youtube04-Eminem.csv' file
 # df = pd.read_json("tweet_words.json")
@@ -17,38 +39,38 @@ tweet_words = [str(i) for i in "tweet_words.csv"]
 # df = [str(i) for i in df]
 # df.describe()
 
-df = tweet_words
+# df = tweet_words
 
-comment_words = ''
-stopwords = set(STOPWORDS)
+# comment_words = ''
+# stopwords = set(STOPWORDS)
 
-# iterate through the csv file
-for val in df:
+# # iterate through the csv file
+# for val in df:
 	
-	# typecaste each val to string
-	val = str(val)
+# 	# typecaste each val to string
+# 	val = str(val)
 
-	# split the value
-	tokens = val.split()
+# 	# split the value
+# 	tokens = val.split()
 	
-	# Converts each token into lowercase
-	for i in range(len(tokens)):
-		tokens[i] = tokens[i].lower()
+# 	# Converts each token into lowercase
+# 	for i in range(len(tokens)):
+# 		tokens[i] = tokens[i].lower()
 	
-	comment_words += " ".join(tokens)+" "
+# 	comment_words += " ".join(tokens)+" "
 
-wordcloud = WordCloud(width = 800, height = 800,
-				background_color ='white',
-				stopwords = stopwords,
-				min_font_size = 10).generate(comment_words)
+# wordcloud = WordCloud(width = 800, height = 800,
+# 				background_color ='white',
+# 				stopwords = stopwords,
+# 				min_font_size = 10).generate(comment_words)
 
-# plot the WordCloud image					
-plt.figure(figsize = (8, 8), facecolor = None)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad = 0)
+# # plot the WordCloud image					
+# plt.figure(figsize = (8, 8), facecolor = None)
+# plt.imshow(wordcloud)
+# plt.axis("off")
+# plt.tight_layout(pad = 0)
 
-plt.show()
+# plt.show()
 
 #DOMO ZONO *sammi do not delete, but also sammi do not understand*
 # %matplotlib inline
