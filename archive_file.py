72ef36d886f348b02ac56abc0717ff7643532c89
Sammi Schumacher <schumachers@emmanuel.edu>
2023-05-07 13:44:34 -0400

    finally have filtered words!

--- archive_file.py
+++ archive_file.py
@@ -8,7 +8,8 @@ from functools import reduce
 import json
 import operator
 import nltk
-
+nltk.download('averaged_perceptron_tagger')
+nltk.download('universal_tagset')
 
 def save_to_json_file(obj, filename):
     f = open(filename, 'w')
@@ -23,81 +24,49 @@ def read_json_file(filename):
     return data
 
 
-def article_filter(df):
-    articles = ['a', 'an', 'the', 'it', 'is', 'its', 'they', 'them', 'their', 'our', 'all', 'any', 'another', 'I', 'both', 'each','we'
-                'us', 'some', 'anything', 'these', 'he', 'him', 'she', 'most', 'you', 'her', 'others', 'himself', 'where', 'whose', 'your']
-    if df in articles:
-        return False
-    else:
-        return True
+# Importing Data Frames 
+reddit_data = pd.read_csv("Reddit_Data.csv")
+twitter_data = pd.read_csv("Twitter_Data.csv")
+
+# Turn Each Post Into Python List 
+reddit_posts = reddit_data['clean_comment'].tolist()
+tweets = twitter_data['clean_text'].tolist()
 
 
-# # Importing Data Frames 
-# reddit_data = pd.read_csv("Reddit_Data.csv")
-# twitter_data = pd.read_csv("Twitter_Data.csv")
+# Turn Each List Into New JSON File of each ind. Word
 
-# # Turn Each Post Into Python List 
-# reddit_posts = reddit_data['clean_comment'].tolist()
-# tweets = twitter_data['clean_text'].tolist()
+print('before string conversion:', len(reddit_posts))
+reddit_posts = [str(i) for i in reddit_posts]
+print('after string conversion:', len(reddit_posts))
+words = reduce(lambda acc, cur: acc + cur.split(), reddit_posts, [])
+print('words after reduce:', len(reddit_posts))
+save_to_json_file(words, 'reddit_words.json')
 
+print('before string conversion:', len(tweets))
+tweets = [str(i) for i in tweets]
+print('after string conversion:', len(tweets))
+words = reduce(lambda acc, cur: acc + cur.split(), tweets, [])
+print('words after reduce:', len(tweets))
+save_to_json_file(words, 'tweet_words.json')
 
-# # Turn Each List Into New JSON File of each ind. Word
-
-# print('before string conversion:', len(reddit_posts))
-# reddit_posts = [str(i) for i in reddit_posts]
-# print('after string conversion:', len(reddit_posts))
-# words = reduce(lambda acc, cur: acc + cur.split(), reddit_posts, [])
-# print('words after reduce:', len(reddit_posts))
-# save_to_json_file(words, 'reddit_words.json')
-
-# print('before string conversion:', len(tweets))
-# tweets = [str(i) for i in tweets]
-# print('after string conversion:', len(tweets))
-# words = reduce(lambda acc, cur: acc + cur.split(), tweets, [])
-# print('words after reduce:', len(tweets))
-# save_to_json_file(words, 'tweet_words.json')
-
-# # Filtering Each JSON File to Eliminate Articles
+# Filtering Each JSON File to Eliminate Articles
 
 tweet_words_json = read_json_file('tweet_words.json')
-# The original style of filtering out articles
-#articles = ['a', 'an', 'the', 'it', 'is', 'its', 'they', 'them', 'their', 'our', 'all', 'any', 'another', 'I', 'both', 'each','we','us','some', 'anything', 'these', 'he', 'him', 'she', 'most', 'you', 'her', 'others', 'himself', 'where', 'whose', 'your']
-#filtered_tweet_words = [word for word in tweet_words_json if word not in articles]
+reddit_words_json =read_json_file('reddit_words.json')
 
 # Filtering out articles using NLTK
 # Using nltk, we can tag each word with the part of speech
 # see https://www.nltk.org/book/ch05.html#tab-universal-tagset
 tagged_tweet_words = nltk.pos_tag(tweet_words_json, tagset='universal')
 parts_not_wanted = ['PRON', 'ADP', 'ADV', 'CONJ', 'DET', 'PRT', '.']
-filtered = [w for w in tagged_tweet_words if w[1] not in parts_not_wanted]
+twitter_filtered = [w for w in tagged_tweet_words if w[1] not in parts_not_wanted]
 # turn back into a regular list of words
-filtered_tweet_words = [w[0] for w in filtered]
+filtered_tweet_words = [w[0] for w in twitter_filtered]
 save_to_json_file(filtered_tweet_words, 'filtered_tweet_words.json')
 
-
-# for item in tweet_words_json:
-#     if item not in articles:
-#         tweet_words_json.append(item)
-# print(tweet_words_json)
-        
-
-
-
-
-
-## filtered words into a file 
-# tweet_words_json = pd.read_json('tweet_words.json')
-# filtered = filter(article_filter, tweet_words_json)
-# print(reduce(filter(article_filter, tweet_words_json), tweet_words_json))
-# for articles in filtered:
-#     print(filtered)
-    # save_to_json_file(filtered, 'tweet_words_filtered.json')
-
-
-
-
-## Sammi's save for later code 
-
-## func to filter out words (chosing not to use bc reduce does this)
-
+tagged_reddit_words = nltk.pos_tag(reddit_words_json, tagset='universal')
+reddit_filtered = [w for w in tagged_reddit_words if w[1] not in parts_not_wanted]
+# turn back into a regular list of words
+filtered_reddit_words = [w[0] for w in reddit_filtered]
+save_to_json_file(filtered_reddit_words, 'filtered_reddit_words.json')
 
